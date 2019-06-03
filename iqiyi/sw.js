e99e9f1599b988ec908e05e9659e0d9d5d00efbb
dean840513 <50709683+dean840513@users.noreply.github.com>
2019-06-03 11:54:29 +0800

    Update sw.js

--- iqiyi/sw.js
+++ iqiyi/sw.js
@@ -2,13 +2,3 @@ self.addEventListener('fetch', function(event) {
   // it can be empty if you just want to get rid of that error
   console.log ("fetch");
 });
-
-
-// 监听 service worker 的 install 事件
-this.addEventListener('install', function (event) {
-    // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
-     event.waitUntil(console.log("install"));
-        // 安装成功后操作 CacheStorage 缓存，使用之前需要先通过 caches.open() 打开对应缓存空间。
-		
-	
-});
