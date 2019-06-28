1a4f073813f3e9a484dffc395698e4aa9a217603
dean840513 <154262267@qq.com>
2019-06-28 11:29:34 +0800

    Update sw.js

--- apps/iqiyi/sw.js
+++ apps/iqiyi/sw.js
@@ -40,6 +40,7 @@ self.addEventListener('activate', (evt) => {
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
+  txt.innerHTML = "请点击地址栏右边的加号按钮安装";
   // CODELAB: Add fetch event handler here.
 
 });
