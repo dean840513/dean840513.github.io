034746f906ebac612a6a3dab664748dbe5f43e0c
dean840513 <154262267@qq.com>
2019-06-28 11:46:02 +0800

    Update sw.js

--- apps/iqiyi/sw.js
+++ apps/iqiyi/sw.js
@@ -21,14 +21,24 @@
 const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
+// CODELAB: Update cache names any time any of the cached files change.
 const FILES_TO_CACHE = [
+  'icon.png',
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
 
-  self.skipWaiting();
+  //self.skipWaiting();
+  
+	// CODELAB: Precache static resources here.
+	evt.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+		  console.log('[ServiceWorker] Pre-caching offline page');
+		  return cache.addAll(FILES_TO_CACHE);
+		})
+	);
 });
 
 self.addEventListener('activate', (evt) => {
