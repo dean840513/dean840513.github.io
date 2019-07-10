f2e6280df6d54810340e21082a95a29662cd9cd6
dean840513 <154262267@qq.com>
2019-07-10 13:08:29 +0800

    Create install - 副本.js

---
+++ apps/install - 副本.js
@@ -0,0 +1,63 @@
+let deferredInstallPrompt = null;
+var timer;
+
+
+// CODELAB: Register service worker.
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../sw.js')
+        .then((reg) => {
+        console.log('Service worker registered.', reg);
+		showProgress();
+        });
+  });
+}
+
+// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', function(evt){
+	// CODELAB: Add code to save event & show the install button.
+	console.log ("beforeinstallprompt");
+	clearInterval(timer);
+	deferredInstallPrompt = evt;
+	document.getElementById('pg').value = 100;
+	document.getElementById('aa').innerHTML = '点击安装';
+	//document.getElementById("aa").click();
+	//installPWA();
+	evt.preventDefault();
+    return false;
+});
+
+function installPWA(evt) {
+	console.log ("installPWA");
+	if (deferredInstallPrompt == null) return false;
+	// CODELAB: Add code to save event & show the install button.
+	deferredInstallPrompt.prompt();
+	// CODELAB: Log user response to prompt.
+	deferredInstallPrompt.userChoice.then((choice) => {
+		  if (choice.outcome === 'accepted') {
+			console.log('User accepted the A2HS prompt', choice);
+			document.location.href = url;
+		  } else {
+			console.log('User dismissed the A2HS prompt', choice);
+		  }
+		  deferredInstallPrompt = null;
+		});
+}
+
+function showProgress(){
+	if (document.getElementById('aa').innerHTML != "已安装") {
+		document.getElementById('aa').innerHTML = "正在检查";
+		timer = setInterval(function(e){
+			if(document.getElementById('pg').value != 100) {
+				if(document.getElementById('pg').value == 99) document.getElementById('pg').value = 0;
+				document.getElementById('pg').value ++;
+			}
+			else {
+				console.log ("showInstallButton");
+				clearInterval(timer);
+				if (document.getElementById('aa').innerHTML != "已安装") document.getElementById('aa').innerHTML = '点击安装';				
+			};
+		},110);	
+	};
+};
+
