a457c7eb7fb5c7061fbf9abfb07faa07a4f1f99c
dean840513 <154262267@qq.com>
2019-07-10 13:08:20 +0800

    Update install.js

--- apps/install.js
+++ apps/install.js
@@ -1,18 +1,6 @@
 let deferredInstallPrompt = null;
 var timer;
 
-
-// CODELAB: Register service worker.
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
-        .then((reg) => {
-        console.log('Service worker registered.', reg);
-		showProgress();
-        });
-  });
-}
-
 // CODELAB: Add event listener for beforeinstallprompt event
 window.addEventListener('beforeinstallprompt', function(evt){
 	// CODELAB: Add code to save event & show the install button.
@@ -60,4 +48,3 @@ function showProgress(){
 		},110);	
 	};
 };
-
