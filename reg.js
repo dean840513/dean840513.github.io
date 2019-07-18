bdaa9e260822dd66e753bbbc160a9d73fde22766
dean840513 <154262267@qq.com>
2019-07-18 17:52:58 +0800

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,22 +1,4 @@
 window.onload = function (){
-	//document.getElementById("main").style.display = 'none';
-	// navigator.serviceWorker.getRegistrations().then(registrations => {
-		// console.log (registrations);
-		// if (typeof(registrations[0]) == "undefined") {
-			//// CODELAB: Register service worker.
-			// if ('serviceWorker' in navigator) {
-			  // window.addEventListener('load', () => {
-				// navigator.serviceWorker.register('./sw.js').then((reg) => {
-					// console.log('Service worker registered.', reg);
-					// SetTime(); 
-				// });
-			  // });
-			// };		
-		// } else {
-			// document.getElementById("main").style.display = 'block';
-			// document.getElementById("loading").style.display = 'none';
-		// };
-	// });
 	navigator.serviceWorker.getRegistrations().then(registrations => {
 		if (typeof(registrations[0]) == "undefined") {
 			navigator.serviceWorker.register('./sw.js').then((reg) => {
