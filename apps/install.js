804d7218d02485ed350af01bb141aa431f628b27
dean840513 <154262267@qq.com>
2019-07-18 09:55:33 +0800

    Update install.js

--- apps/install.js
+++ apps/install.js
@@ -1,62 +1,34 @@
-let deferredInstallPrompt = null;
-var timer;
+window.onload = function (){
+	let deferredPrompt;
+	const addBtn = document.querySelector('#aa');
+	addBtn.style.display = 'none';
 
-// CODELAB: Register service worker.
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
-        .then((reg) => {
-        console.log('Service worker registered.', reg);
-		showProgress();
-        });
-  });
-}
+	window.addEventListener('beforeinstallprompt', (e) => {
+	  // Prevent Chrome 67 and earlier from automatically showing the prompt
+	  e.preventDefault();
+	  // Stash the event so it can be triggered later.
+	  deferredPrompt = e;
+	  // Update UI to notify the user they can add to home screen
+	  addBtn.style.display = 'block';
 
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', function(evt){
-	// CODELAB: Add code to save event & show the install button.
-	console.log ("beforeinstallprompt");
-	clearInterval(timer);
-	deferredInstallPrompt = evt;
-	document.getElementById('pg').value = 100;
-	document.getElementById('aa').innerHTML = '点击安装';
-	//document.getElementById("aa").click();
-	//installPWA();
-	evt.preventDefault();
-    return false;
-});
-
-function installPWA(evt) {
-	console.log ("installPWA");
-	if (deferredInstallPrompt == null) return false;
-	// CODELAB: Add code to save event & show the install button.
-	deferredInstallPrompt.prompt();
-	// CODELAB: Log user response to prompt.
-	deferredInstallPrompt.userChoice.then((choice) => {
-		  if (choice.outcome === 'accepted') {
-			console.log('User accepted the A2HS prompt', choice);
-			document.location.href = url;
-		  } else {
-			console.log('User dismissed the A2HS prompt', choice);
-		  }
-		  deferredInstallPrompt = null;
-		});
-}
-
-function showProgress(){
-	if (document.getElementById('aa').innerHTML != "已安装") {
-		document.getElementById('aa').innerHTML = "正在检查";
-		timer = setInterval(function(e){
-			if(document.getElementById('pg').value != 100) {
-				if(document.getElementById('pg').value == 99) document.getElementById('pg').value = 0;
-				document.getElementById('pg').value ++;
+	  addBtn.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+			  console.log('User accepted the A2HS prompt');
+			} else {
+			  console.log('User dismissed the A2HS prompt');
 			}
-			else {
-				console.log ("showInstallButton");
-				clearInterval(timer);
-				if (document.getElementById('aa').innerHTML != "已安装") document.getElementById('aa').innerHTML = '点击安装';				
-			};
-		},110);	
-	};
-};
-
+			deferredPrompt = null;
+		  });
+	  });
+	});
+	
+	navigator.serviceWorker.getRegistrations().then(registrations => {
+		if (typeof(registrations[0]) == "undefined") alert ("no");
+	});
+}
