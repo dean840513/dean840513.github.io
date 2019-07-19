4916c3a9e592d6925fb47e3ab9589250549733cc
dean840513 <154262267@qq.com>
2019-07-19 11:01:12 +0800

    Update install.js

--- apps/install.js
+++ apps/install.js
@@ -4,28 +4,20 @@ window.onload = function (){
 	var url = document.location.href;
 
 	window.addEventListener('beforeinstallprompt', (e) => {
-	  // Prevent Chrome 67 and earlier from automatically showing the prompt
 	  e.preventDefault();
-	  // Stash the event so it can be triggered later.
 	  deferredPrompt = e;
-	  // Update UI to notify the user they can add to home screen
 	  $("#pre").hide();
-	  //install.style.display = 'block';
 	  $("#install").fadeIn(1000);
 
 	  install.addEventListener('click', (e) => {
-		// hide our user interface that shows our A2HS button
-		//install.style.display = 'none';
-		// Show the prompt
-		deferredPrompt.prompt();
-		// Wait for the user to respond to the prompt
-		deferredPrompt.userChoice.then((choiceResult) => {
-			if (choiceResult.outcome === 'accepted') {
-			  console.log('User accepted the A2HS prompt');
-			} else {
-			  console.log('User dismissed the A2HS prompt');
-			}
-			deferredPrompt = null;
+			deferredPrompt.prompt();
+			deferredPrompt.userChoice.then((choiceResult) => {
+				if (choiceResult.outcome === 'accepted') {
+				  console.log('User accepted the A2HS prompt');
+				} else {
+				  console.log('User dismissed the A2HS prompt');
+				}
+				deferredPrompt = null;
 		  });
 	  });
 	});
