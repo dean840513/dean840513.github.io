bb7a8f2ce0466847be9f702d7e7e8e038659fece
dean840513 <154262267@qq.com>
2019-07-22 17:54:37 +0800

    Update install.js

--- apps/install.js
+++ apps/install.js
@@ -3,7 +3,6 @@ window.onload = function(){
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.register('../sw.js').then((reg) => {
 			console.log('Service worker registered.', reg);
-			document.getElementById("pre").innerHTML = "安装中。。。请稍候。";
 		});
 	};
 };
