85e71adf326e6ea07f89a1c1ee5f53c074d77c4f
dean840513 <154262267@qq.com>
2019-07-22 16:20:26 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#pre").hide();
+	$("#main").fadeIn(1000);
+	
+	$("a").click(function(){
+		var r = confirm("是否安装应用？" + this.name);
+		if (r == false) 
+			return false;	
+	});
+	
+	// CODELAB: Register service worker.
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./app/sw.js').then((reg) => {console.log('Service worker registered.', reg);});
+	};
+});
