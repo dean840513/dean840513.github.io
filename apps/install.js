401ac67673a6dac7c2a785e2fbb1ece050bc5ad1
dean840513 <154262267@qq.com>
2019-07-22 16:30:04 +0800

    Update install.js

--- apps/install.js
+++ apps/install.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	// CODELAB: Register service worker.
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('./app/sw.js').then((reg) => {
+		navigator.serviceWorker.register('../sw.js').then((reg) => {
 			console.log('Service worker registered.', reg);
 		});
 	};
