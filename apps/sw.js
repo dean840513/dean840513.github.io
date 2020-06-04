adae5df4083fccda85d8888d95463f3311b988d8
dean840513 <50709683+dean840513@users.noreply.github.com>
2020-06-04 18:54:40 +0800

    Update sw.js

--- apps/sw.js
+++ apps/sw.js
@@ -16,16 +16,8 @@ self.addEventListener('fetch', e => {
         caches.match(e.request).then(res => {
             if (res) {
                 return res
-            } else {
-                // 如果没有缓存，用fetch方法从网络上获取资源
-                fetch(url).then(res => {
-                    if (res) {
-                        //对于新请求的资源缓存到cachestorage中
-                    } else {
-                        //提示
-                    }
-                })
-            }
+            } 
+		return fetch(e.request);
         })
-    )
-})
+	)
+})	
