1ecdde5eac65a5a553cdddd91a972333216ce5e2
dean840513 <50709683+dean840513@users.noreply.github.com>
2020-06-04 18:37:55 +0800

    Update sw.js

--- apps/sw.js
+++ apps/sw.js
@@ -1,3 +1,31 @@
-self.addEventListener('fetch', (evt) => {
-	console.log ('fetch');
-});
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('app-v1')
+            .then(cache => {
+                console.log('open cache')
+                return cache.addAll([
+                    'go.html'
+                ])
+            })
+    )
+})
+
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            if (res) {
+                return res
+            } else {
+                // 如果没有缓存，用fetch方法从网络上获取资源
+                fetch(url).then(res => {
+                    if (res) {
+                        //对于新请求的资源缓存到cachestorage中
+                    } else {
+                        //提示
+                    }
+                })
+            }
+        })
+    )
+})
