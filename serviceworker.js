5fd824c36a47361437bd7dc8291b8d3d6b1dc750
dean840513 <50709683+dean840513@users.noreply.github.com>
2020-06-04 17:42:38 +0800

    Delete serviceworker.js

--- serviceworker.js
+++
@@ -1,6 +0,0 @@
-// CODELAB: Register service worker.
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js').then(function (reg) {
-		console.log('Service worker registered.', reg);
-	});
-};
