f186388a9b91045c60ce825cc34956becbe2883b
dean840513 <50709683+dean840513@users.noreply.github.com>
2020-06-04 15:51:43 +0800

    Delete install.js

--- apps/install.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	// CODELAB: Register service worker.
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('../sw.js').then((reg) => {
-			console.log('Service worker registered.', reg);
-		});
-	};
-};
